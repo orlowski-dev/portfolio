01c26e358d9f0b08be7d477a65430e798e53054a
orlowski-dev <orlowski.dev@gmail.com>
2023-11-26 10:22:39 +0100

    Added Hero section component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import AppHeader from "@/components/AppHeader";
+import Hero from "@/components/Hero";
 
 export default function App() {
   return (
     <>
       <AppHeader />
       <main>
-        <h1>Portfolio</h1>
+        <Hero />
       </main>
     </>
   );
---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import "./Hero.scss";
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <div className="container">
+        <h1>Patryk Orłowski</h1>
+        <p>&#8212; Frontend Developer &#8212;</p>
+      </div>
+    </div>
+  );
+}
