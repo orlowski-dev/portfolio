2ce72f9d67b1c8378b1ac2296afa8792e9b0f3fc
orlowski-dev <orlowski.dev@gmail.com>
2023-11-26 09:04:41 +0100

    Added AppHeader component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import AppHeader from "@/components/AppHeader";
+
 export default function App() {
   return (
-    <main>
-      <h1>Portfolio</h1>
-    </main>
+    <>
+      <AppHeader />
+      <main>
+        <h1>Portfolio</h1>
+      </main>
+    </>
   );
 }
---
+++ components/AppHeader.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import "./AppHeader.scss";
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa6";
+
+export default function AppHeader() {
+  return (
+    <header className="app-header">
+      <div className="container">
+        <div>
+          <Image src="/logo.svg" height={24} width={42} alt="logo" priority />
+        </div>
+        <div className="links">
+          <Link
+            href="https://github.com/orlowski-dev"
+            target="_blank"
+            title="Profil GitHub"
+            className="gh-link"
+          >
+            <FaGithub />
+          </Link>
+          <Link href="/" target="_blank" className="cv-link">
+            Moje CV
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
